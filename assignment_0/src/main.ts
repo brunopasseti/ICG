1d6cdc4ad86987d68ab66c9674b6a5806b296121
brunopasseti <brunopassetidev@gmail.com>
2021-03-15 19:06:35 -0300

    Adding drawing to canvas

--- assignment_0/src/main.ts
+++ assignment_0/src/main.ts
@@ -2,15 +2,17 @@ import { Vector } from "./Vector";
 import { Matrix } from "./Matrix";
 import { quicksort } from "./Algorithm";
 import * as THREE from "three";
-// alert("Hello World") // 1
+import { formatDiagnosticsWithColorAndContext } from "typescript";
 
+// alert("Hello World") // 1
+////////////////////////////////////////////////////////////
 const a: Array<number> = [];
 for(let i = 0; i < 10; i++){
     a.push(Math.floor(Math.random() * (100 - 0)));
 } // 2
 const sorted: Array<number> = quicksort(a, 0, 9);
 console.log(sorted); // 3
-
+////////////////////////////////////////////////////////////
 const v1 = new Vector(1,2,3);
 const v2 = new Vector(3,2,1);
 v1.add(v2);
@@ -30,7 +32,27 @@ m2.printAsTable();
 const m3 = m2.mulMatrix(m1);
 m3.printAsTable()
 console.log("Det m2: ", m2.determinant);
+////////////////////////////////////////////////////////////
+const canvasJS = <HTMLCanvasElement> document.getElementById("canvasDesenhoJS");
+if(canvasJS){
+    const ctx = canvasJS.getContext("2d");
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(0, 0, 150, 75);
+    ctx.fillStyle = "#0000FF";
+    ctx.fillRect(0,40, 130, 200);
+    ctx.fillStyle = "#AAAAAA";
+    ctx.fillRect(30,20, 50, 50);
+    ctx.fillStyle = "#FFAA22";
+    ctx.fillRect(100, 200, 75, 75);
+    ctx.fillStyle = "#2AAFF";
+    ctx.fillRect(200,200, 100, 100);
+    ctx.fillStyle = "#BBAA99";
+    ctx.fillRect(50,250, 200, 50);
+    ctx.fillStyle = "#11FF22";
+    ctx.fillRect(200,10, 50, 50);
 
+}
+////////////////////////////////////////////////////////////
 function createCamera(){
     const fov = 75;
     const aspect = 2;  
