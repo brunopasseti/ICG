aff0bc8ebd0e9e9323c3abe25fb09b4df975f9b9
Rodrigo Ramalho <rodrigoramalho.dev@gmail.com>
2021-03-23 19:29:01 -0300

    Modifications in the MidPointAlgorithm to make it generic for all octants. Add DrawTriangle function.

--- assignment_1/canvas.js
+++ assignment_1/canvas.js
@@ -16,41 +16,69 @@ class Canvas {
     }
 
     getWidth(){
-        return this.canvas.width;
+      return this.canvas.width;
     }
 
     getHeight(){
-        return this.canvas.height;
+      return this.canvas.height;
     }
 
     MidPointLineAlgorithm(xi, yi, xf, yf, color_i, color_f){
-        const dx = xf-xi, dy = yf-yi;
-        let delta = 2*dy - dx;
-        const incr_east = 2*dy;
-        const incr_northwest = 2*(dy - dx);
+      let dx = Math.abs(xf-xi); 
+      let dy = Math.abs(yf-yi);
 
-        let x = xi, y = yi;
-        let r = color_i[0], g = color_i[1], b = color_i[2];
-        const varR = (color_f[0] - color_i[0])/dx
-        const varG = (color_f[1] - color_i[1])/dx
-        const varB = (color_f[2] - color_i[2])/dx
-        this.putPixel(x, y, [r,g,b])
-        while(x < xf){
-            r += varR;
-            g += varG;
-            b += varB;
-            if(delta <= 0){
-                delta+=incr_east;
-                x++
-            }else{  
-                delta+=incr_northwest;
-                x++;
-                y++;
-            }
-            console.log(`putting pixel (${x},${y} with color [${r}, ${g}, ${b}]`);
-            this.putPixel(x,y, [r,g,b])
-        }
-        this.putPixel(x,y, color_f)
+      let isAxisChanged = false;
+
+      if(dy > dx){
+        let aux = dx;
+        dx = dy;
+        dy = aux;
+        isAxisChanged = true;
+      }
+      
+      let delta = 2*dy - dx;
+      const incr_east = 2*dy;
+      const incr_northwest = 2*(dy - dx);
+
+      let signalX = Math.sign((xf - xi));
+      let signalY = Math.sign((yf - yi));
+
+      let x = xi, y = yi;
+      let r = color_i[0], g = color_i[1], b = color_i[2];
+      const varR = (color_f[0] - color_i[0])/dx
+      const varG = (color_f[1] - color_i[1])/dx
+      const varB = (color_f[2] - color_i[2])/dx
+
+      this.putPixel(x, y, [r,g,b])
+      let count = 0;
+      while(count < dx){
+          r += varR;
+          g += varG;
+          b += varB;
+          if(delta <= 0){
+              delta+=incr_east;
+              if(isAxisChanged)
+                y += signalY;
+              else
+                x += signalX;
+          }else{  
+              delta+=incr_northwest;
+              x += signalX;
+              y += signalY;
+          }
+
+          console.log(`putting pixel (${x},${y} with color [${r}, ${g}, ${b}]`);
+          this.putPixel(x,y, [r,g,b])
+          count++;
+      }
+      
+      this.putPixel(xf,yf, color_f)
+    }
+
+    DrawTriangle(x0, y0, x1, y1, x2, y2, color_0, color_1, color_2){
+      this.MidPointLineAlgorithm(x0, y0, x1, y1, color_0, color_1);
+      this.MidPointLineAlgorithm(x1, y1, x2, y2, color_1, color_2)
+      this.MidPointLineAlgorithm(x2, y2, x0, y0, color_2, color_0)
     }
   }
   
--- assignment_1/main.js
+++ assignment_1/main.js
@@ -1,3 +1,5 @@
 const teste = new Canvas("draw_canvas");
 teste.clear()
-teste.MidPointLineAlgorithm(25, 30, 100, 80, [255,0,0,255], [255,255,0,255])
+teste.MidPointLineAlgorithm(95, 40, 120, 65, [255,0,0,255], [255,255,0,255])
+teste.MidPointLineAlgorithm(70, 100, 20, 120, [0,255,0,255], [255,255,0,255])
+teste.DrawTriangle(25,30,50,100,100,15,[255,0,0,255], [0,0,255,255], [0,255,0,255])
